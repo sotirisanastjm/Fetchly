0cf9f067c33d752a0eaeb81ca443a45ad8e08f6d
Sotirios Anastasiadis <sotiris.anastjm@gmail.com>
2025-08-01 02:23:32 +0300

    Update ArticleContext.tsx

--- fetchly/src/context/ArticleContext.tsx
+++ fetchly/src/context/ArticleContext.tsx
@@ -6,47 +6,45 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 const ArticlesContext = createContext<ArticlesContextProps | undefined>(undefined);
 
 export const ArticlesProvider: React.FC<{ children: React.ReactNode; initialArticles?: Article[], tag: string }> = ({
-  children,
-  initialArticles = [],
-  tag,
+    children,
+    initialArticles = [],
+    tag,
 }) => {
-  const [articles, setArticles] = useState<Article[]>(initialArticles);
-  const [loading, setLoading] = useState(initialArticles.length === 0);
-  const [error, setError] = useState<string | null>(null);
-  const [activeTag, setActiveTag] = useState<string | null>(tag);
+    const [articles, setArticles] = useState<Article[]>(initialArticles);
+    const [loading, setLoading] = useState(initialArticles.length === 0);
+    const [error, setError] = useState<string | null>(null);
+    const [activeTag, setActiveTag] = useState<string | null>(tag);
 
-  const tags = ['angular', 'nextjs', 'react', 'typescript', 'javaScript', 'tailwind', 'nodeJs', 'webdev'];
+    const tags = ['angular', 'nextjs', 'react', 'typescript', 'javascript', 'tailwind', 'nodejs', 'webdev'];
 
-  const fetchArticlesByTag = async (tag: string, pageSize: number) => {
-    setLoading(true);
-    setActiveTag(tag);
-    try {
-      const res = await fetchArticles(tag, pageSize);
-      if (!res.ok) throw new Error("Failed to fetch articles");
-      const data: Article[] = await res.json();
-      setArticles(data);
-      setError(null);
-    } catch (err) {
-      if (err instanceof Error) {
-        setError(err.message);
-      } else {
-        setError("An unknown error occurred");
-      }
-      setArticles([]);
-    } finally {
-      setLoading(false);
-    }
-  };
+    const fetchArticlesByTag = async (tag: string, pageSize: number) => {
+        setLoading(true);
+        setActiveTag(tag);
+        try {
+            const res = await fetchArticles(tag, pageSize);
+            setArticles(res);
+            setError(null);
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            } else {
+                setError("An unknown error occurred");
+            }
+            setArticles([]);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-  return (
-    <ArticlesContext.Provider value={{ articles, loading, error, activeTag, tags, fetchArticlesByTag }}>
-      {children}
-    </ArticlesContext.Provider>
-  );
+    return (
+        <ArticlesContext.Provider value={{ articles, loading, error, activeTag, tags, fetchArticlesByTag }}>
+            {children}
+        </ArticlesContext.Provider>
+    );
 };
 
 export const useArticles = () => {
-  const context = useContext(ArticlesContext);
-  if (!context) throw new Error("useArticles must be used within an ArticlesProvider");
-  return context;
+    const context = useContext(ArticlesContext);
+    if (!context) throw new Error("useArticles must be used within an ArticlesProvider");
+    return context;
 };
