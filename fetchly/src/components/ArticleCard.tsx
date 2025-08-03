6f5250bd7f9b11b7ba8a79d2af98209a0538b8c6
Sotirios Anastasiadis <sotiris.anastjm@gmail.com>
2025-08-03 16:59:25 +0300

    Update ArticleCard.tsx

--- fetchly/src/components/ArticleCard.tsx
+++ fetchly/src/components/ArticleCard.tsx
@@ -79,7 +79,7 @@ export default function ArticleCard({ article }: ArticleCardProps) {
                     </div>
                 </div>
                 <button className='absolute hover:cursor-pointer right-[32px] top-[-16px]' onClick={toggleSave} title={isSaved ? 'Remove from saved' : 'Save article'}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#8fabcc" className={`size-10 hover:scale-[1.08] hover:opacity-[1] hover:stroke-2 uration-300 transition-all ${isSaved ? 'fill-[#8fabcc]' : 'fill-[#1c262e] opacity-[0.6]'}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#8fabcc" className={`size-10 hover:scale-[1.08] hover:opacity-[1] hover:stroke-2 uration-300 transition-all ${isSaved ? 'fill-[#8fabcc]' : 'fill-[#1c262e]'}`}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                     </svg>
                 </button>
